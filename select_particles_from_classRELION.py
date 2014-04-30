252645a440d713e3388f1387155a666a01e40d52
Michael Cianfrocco <michael.a.cianfrocco@gmail.com>
2014-04-30 18:58:22 -0400

    Added option for more than one class to select

--- select_particles_from_classRELION.py
+++ select_particles_from_classRELION.py
@@ -12,11 +12,11 @@ from os import system
 #=========================
 def setupParserOptions():
         parser = optparse.OptionParser()
-        parser.set_usage("%prog -i [file].star --class=[classNum]")
+        parser.set_usage("%prog -i [file].star --class=[classNum1],[classNum2],..")
  	parser.add_option("-i",dest="star",type="string",metavar="FILE",
                 help="Data file (.star) from RELION 3D classification")
-        parser.add_option("--class",dest="classNumber",type="int", metavar="INT",
-                help="Class number of particle data that you would like extracted")
+        parser.add_option("--class",dest="classNumber",type="string", metavar="STRING",
+                help="Class number(s) of particle data that you would like extracted. (Comma separated if > 1 class)")
 	parser.add_option("-d", action="store_true",dest="debug",default=False,
                 help="debug")
 	options,args = parser.parse_args()
@@ -32,7 +32,7 @@ def setupParserOptions():
 	return params
 
 #=============================
-def selectParts(params):
+def selectPartsOneClass(params):
 
 	#Check if input exists
 	if os.path.exists(params['star']) is False:
@@ -40,12 +40,12 @@ def selectParts(params):
                 sys.exit()
 
         #Check if output exists
-        if os.path.exists('%s_class%02d.star' %(params['star'][:-5],params['classNumber'])) is True:
-                print '%s_class%02d.star already exists, exiting...' %(params['star'][:-5],params['classNumber'])
+        if os.path.exists('%s_class%02d.star' %(params['star'][:-5],int(params['classNumber']))) is True:
+                print '%s_class%02d.star already exists, exiting...' %(params['star'][:-5],int(params['classNumber']))
                 sys.exit()
 
 	starfile = open(params['star'],'r')
-	outfile = open('%s_class%02d.star' %(params['star'][:-5],params['classNumber']),'w')
+	outfile = open('%s_class%02d.star' %(params['star'][:-5],int(params['classNumber'])),'w')
 
 	#File in header info
 	outfile.write('\n')
@@ -65,11 +65,51 @@ def selectParts(params):
 			continue
 		if params['debug'] is True:
 			print lineparse[6]
-		if float(lineparse[6]) == params['classNumber']:
+		if float(lineparse[6]) == int(params['classNumber']):
 			outfile.write('%s\t%s\t%s\t%s\t%s\t%s\n'%(lineparse[0],lineparse[6],lineparse[8],lineparse[9],lineparse[10],lineparse[11]))	
+#=============================
+def selectPartsMoreThanOne(params):
+
+        #Check if input exists
+        if os.path.exists(params['star']) is False:
+                print '%s does not exist, exiting...' %(params['star'])
+                sys.exit()
+
+        #Check if output exists
+        if os.path.exists('%s_selClasses.star' %(params['star'][:-5])) is True:
+                print '%s_selClasses.star already exists, exiting...' %(params['star'][:-5])
+                sys.exit()
+
+        starfile = open(params['star'],'r')
+        outfile = open('%s_selClasses.star' %(params['star'][:-5]),'w')
+
+        #File in header info
+        outfile.write('\n')
+        outfile.write('data_images\n')
+        outfile.write('\n')
+        outfile.write('loop_\n')
+        outfile.write('_rlnImageName\n')
+        outfile.write('_rlnClassNumber\n')
+        outfile.write('_rlnMagnificationCorrection\n')
+        outfile.write('_rlnLogLikeliContribution\n')
+        outfile.write('_rlnMaxValueProbDistribution\n')
+        outfile.write('_rlnNrOfSignificantSamples\n')
+
+        for line in starfile:
+                lineparse = line.split()
+                if len(lineparse) < 3:
+                        continue
+                if params['debug'] is True:
+                        print lineparse[6]
+			print params['classNumber'].split(',')
+                if lineparse[6] in  params['classNumber'].split(','):
+                        outfile.write('%s\t%s\t%s\t%s\t%s\t%s\n'%(lineparse[0],lineparse[6],lineparse[8],lineparse[9],lineparse[10],lineparse[11])) 	
 
 #==============================
 if __name__ == "__main__":
 
 	params=setupParserOptions()
-	selectParts(params)	
+	if len(params['classNumber'].split(',')) == 1: 
+		selectPartsOneClass(params)	
+	if len(params['classNumber'].split(',')) > 1: 
+                selectPartsMoreThanOne(params)
