0d06de69e21084e22d836070a6afde565d0048a0
Michael A. Cianfrocco <michael.a.cianfrocco@gmail.com>
2015-08-19 17:15:07 -0500

    Finished writing script that bins relion stacks and renames star files correctly

--- bin_particleStack_starFile.py
+++ bin_particleStack_starFile.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import shutil
 import optparse
 from sys import *
 import os,sys,re
@@ -17,6 +18,8 @@ def setupParserOptions():
                 help="Data file (.star) for RELION particle stack")
         parser.add_option("--bin",dest="bin",type="int", metavar="INTEGER",
                 help="Binning factor for particle data set")
+	parser.add_option("--apix",dest="apix",type="float", metavar="FLOAT",
+                help="Pixel size for input particles")
 	parser.add_option("--oext",dest="oext",type="string", metavar="STRING",                
 		help="Output extension for particle stacks and star files")
         parser.add_option("-d", action="store_true",dest="debug",default=False,
@@ -24,7 +27,7 @@ def setupParserOptions():
         options,args = parser.parse_args()
         if len(args) > 0:
                 parser.error("Unknown commandline options: " +str(args))
-        if len(sys.argv) < 5:
+        if len(sys.argv) < 6:
                 parser.print_help()
                 sys.exit()
         params={}
@@ -40,19 +43,26 @@ def checkConflicts(params):
 		print 'File %s does not exist. Exiting' %(params['star'])
 		sys.exit()
 
+	if os.path.exists('%s_%s.star' %(params['star'][:-5],params['oext'])):
+		print 'File %s_%s.star already exists. Exiting.' %(params['star'][:-5],params['oext'])
+		sys.exit()
+
 #==============================
 def binStack(params):
 	
 	#Open star file and get particle stack, X & Y coordinates
 
 	f1=open(params['star'],'r')
+	o1=open('%s_%s.star' %(params['star'][:-5],params['oext']),'w')
 
 	for line in f1:
 
 		if len(line.split()) < 3:
-                        continue
-
+                        o1.write(line)
+			continue
+		linesplit=line.split()	
 		stack=(line.split()[3].split('@')[1])
+		number=(line.split()[3].split('@')[0])
 		xcoord=line.split()[1]
 		ycoord=line.split()[2]
 		pixelsize=line.split()[-2]
@@ -60,15 +70,30 @@ def binStack(params):
 		if params['debug'] is True:
 			print stack
 
-		if os.path.exists('%s_%s.mrcs' %(stack[:-4],params['oext'])):
+		if os.path.exists('%s_%s.mrcs' %(stack[:-5],params['oext'])):
 			if params['debug'] is True:
-				print '%s_%s.mrcs already exists, skipping' %(stack[:-4],params['oext'])
+				print '%s_%s.mrcs already exists, skipping' %(stack[:-5],params['oext'])
 			
-		if not os.path.exists('%s_%s.mrcs' %(stack[:-4],params['oext'])):
+		if not os.path.exists('%s_%s.mrcs' %(stack[:-5],params['oext'])):
 			#relion image handler to bin stack
-						
+			cmd='relion_image_handler --i %s --o %s_%s.mrcs --angpix %f --rescale_angpix %f' %(stack,stack[:-5],params['oext'],params['apix'],float(params['bin'])*params['apix'])
+			if params['debug'] is True:
+				print cmd
+			subprocess.Popen(cmd,shell=True).wait()							
 
 		#Write new coordinates, stack name, and new detector size into new star file
+		seq=("%s"%(number),"@","%s_%s.mrcs"%(stack[:-5],params['oext']))
+		newstack=''.join(seq)
+		linesplit[3]=newstack
+		linesplit[1]=str(round(float(xcoord)/float(params['bin'])))
+		linesplit[2]=str(round(float(ycoord)/float(params['bin'])))
+		linesplit[-2]=str(float(pixelsize)*float(params['bin']))
+		linesplit='\t'.join(linesplit)
+	
+		o1.write('%s\n' %(linesplit))
+
+		if not os.path.exists('%s_%s.star' %(stack[:-5],params['oext'])):
+			shutil.copy('%s.star' %(stack[:-5]),'%s_%s.star' %(stack[:-5],params['oext']))
 
 	f1.close()
 
